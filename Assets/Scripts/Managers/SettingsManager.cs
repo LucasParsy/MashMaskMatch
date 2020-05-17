f5a96d9c5cb760939d20bc442a81c418263bec25
Lucas Parsy <lucas.parsy@epitech.eu>
2020-05-18 01:41:50 +0200

    added credit page on the settings.
    with button link to our Github's

--- Assets/Scripts/Managers/SettingsManager.cs
+++ Assets/Scripts/Managers/SettingsManager.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
@@ -11,5 +14,29 @@ public class SettingsManager : MonoBehaviour
     {
         SaveManager.Instance.ResetProgression();
         OnClickReturnBtn();
+    }
+
+
+    public IEnumerator creditsClickCorout(bool isEnter)
+    {
+        float transitionDuration = 1;
+        int direction = isEnter ? 0 : 2000;
+        float fade = isEnter ? 0.35f : 0;
+
+        GameObject go = GameObject.Find("creditsDisplay");
+        go.transform.DOLocalMoveX(direction, transitionDuration).SetEase(Ease.Linear);
+
+        Image bg = GameObject.Find("BackgroundFaderBlocker").GetComponent<Image>();
+        yield return bg.DOFade(fade, transitionDuration).WaitForCompletion();
+        bg.raycastTarget = isEnter;
+    }
+    public void onClickCredits(bool isEnter)
+    {
+        StartCoroutine(creditsClickCorout(isEnter));
+    }
+
+    public void OpenURL(string url)
+    {
+        Application.OpenURL(url);
     }
 }
